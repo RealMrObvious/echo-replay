8b924115f47c1e45ea351b31af057524cf484fe5
A. K. <48920112+RealMrObvious@users.noreply.github.com>
2026-08-16 02:44:15 -0400

    add: created brand new gui (wip)

--- src/gui/gui_main.py
+++ src/gui/gui_main.py
@@ -1,23 +1,38 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QHBoxLayout
+from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtGui import QIcon
 from .tray import Tray
+from .ui_main_window import Ui_MainWindow
+
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.setWindowTitle("Main Window")
-        self.setGeometry(100, 100, 400, 300)
-        self.hide()
+        self.setWindowIcon(QIcon("media/icon.png"))
 
-        self.setCentralWidget(CenterPane())
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.hide()
 
         self.tray = Tray(QApplication.instance(), self)
 
-class CenterPane(QWidget):
-    # While this class does not currently contain much what it does 
-    # contain is essential and it allows for expansion later on --
-    # for example what if instead of a single textbox one wants a
-    # tree and a list view that would be handled here
-    def __init__(self):
-        QWidget.__init__(self)
-        hbox = QHBoxLayout(self)
+    def open_gallery(self):
+        self.open_tab(self.ui.gallery_tab)
+
+    def open_settings(self):
+        self.open_tab(self.ui.settings_tab)
+
+    def open_add_games(self):
+        self.open_tab(self.ui.add_games_tab)
+
+    def open_logs(self):
+        self.open_tab(self.ui.logs_tab)
+
+    def open_tab(self, tab):
+        self.ui.tabWidget.setCurrentWidget(tab)
+        self.show()
+
+        # Bring the window to the front
+        self.raise_()
+        self.activateWindow()
